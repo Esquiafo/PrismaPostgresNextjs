88c0c10f8843950e378e0b4ea87b95f518a18c27
Esquiafo <leocapone@hotmail.com>
2023-02-05 15:29:57 -0300

    152905022023

--- pages/api/index.ts
+++ pages/api/index.ts
@@ -4,6 +4,10 @@ export default async function handler(
     req: NextApiRequest,
     res: NextApiResponse<any>
 ){
+    if (req.method=="GET") {
     res.status(200).json({status: 200})
+    }else{
+    res.status(200).json({status: 200})
+}
 }
   
