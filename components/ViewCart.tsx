744e337cc41173da2ae13b5423b6228ff21853c7
Esquiafo <leocapone@hotmail.com>
2023-03-13 18:05:41 -0300

    180513032023

--- components/ViewCart.tsx
+++ components/ViewCart.tsx
@@ -71,10 +71,16 @@ export default function CartView() {
      let products = cart.items.map(product => {
         console.log(product)
 
-          
           return (
-      
+           
              <div key={product.id}>
+              <h3>Eliminar item: </h3>
+              <button onClick={()=>deleteItem(product.id)} type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-full text-sm p-2.5 text-center inline-flex items-center mr-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
+              <path strokeLinecap="round" strokeLinejoin="round" d="M9 15L3 9m0 0l6-6M3 9h12a6 6 0 010 12h-3" />
+              </svg>
+              <span className="sr-only">Icon description</span>
+              </button>
               <div className="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
           <img className="object-cover w-full rounded-t-lg h-24 md:h-auto md:w-48 md:rounded-none md:rounded-l-lg" src={`${product.image}`} alt={`${['image']} image`} />
           <div className="flex flex-col justify-between p-4 leading-normal">
@@ -110,6 +116,14 @@ export default function CartView() {
     return (
     
       <div >
+        <h3>Eliminar todo: </h3>
+              <button onClick={()=>deleteAll()}type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-full text-sm p-2.5 text-center inline-flex items-center mr-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
+  <path strokeLinecap="round" strokeLinejoin="round" d="M18.364 18.364A9 9 0 005.636 5.636m12.728 12.728A9 9 0 015.636 5.636m12.728 12.728L5.636 5.636" />
+</svg>
+
+<span className="sr-only">Icon description</span>
+              </button>
       {products}
      </div>
     );
