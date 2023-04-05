4d3dcbc5ef963aa83999da653e4022ed4e25aa95
Esquiafo <leocapone@hotmail.com>
2023-04-05 01:23:51 -0300

    012305042023
    TEST CORS

--- next.config.js
+++ next.config.js
@@ -3,17 +3,4 @@ module.exports = {
   env: {
     API_KEY: process.env.API_KEY
   },
-  async headers() {
-    return [
-      {
-        source: '/api/:path*',
-        headers: [
-          { key: 'Access-Control-Allow-Origin', value: '*' },
-          { key: 'Access-Control-Allow-Methods', value: 'GET, POST, DELETE, PUT' },
-          { key: 'Access-Control-Allow-Headers', value: 'Content-Type' },
-          { key: 'Access-Control-Allow-Credentials', value: 'true' }
-        ]
-      }
-    ];
-  }
 };
--- pages/api/login/index.ts
+++ pages/api/login/index.ts
@@ -1,23 +1,50 @@
-import { corsHandler } from "../../../middlewares/cors";
+
 import { PrismaClient } from "@prisma/client";
 import type { NextApiRequest, NextApiResponse } from "next";
 import bcrypt from "bcryptjs";
 import jwt, { Secret } from "jsonwebtoken";
-
+import Cors from 'cors';
 const prisma = new PrismaClient();
 
+// Initializing the cors middleware
+// You can read more about the available options here: https://github.com/expressjs/cors#configuration-options
+const cors = Cors({
+  methods: ['POST', 'GET', 'HEAD'],
+})
+
+// Helper method to wait for a middleware to execute before continuing
+// And to throw an error when an error happens in a middleware
+function runMiddleware(
+  req: NextApiRequest,
+  res: NextApiResponse,
+  fn: Function
+) {
+  return new Promise((resolve, reject) => {
+    fn(req, res, (result: any) => {
+      if (result instanceof Error) {
+        return reject(result)
+      }
+
+      return resolve(result)
+    })
+  })
+}
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<any>
 ) {
-  await corsHandler(req, res); // usa el middleware
-
+  await runMiddleware(req, res, cors)
+  // Rest of the API logic
   if (req.method === "POST") {
     const user = await checkUser(req);
     res.status(200).json(user);
     return user;
   }
-  return res.status(401).send("You are not authorized to call this API");
+}
+ {
+ 
+
+
 }
 
 async function checkUser(req: NextApiRequest): Promise<any> {
