fc86a40e102b76eec5f7b6d982e5d5428553a7b6
Esquiafo <leocapone@hotmail.com>
2023-04-06 01:08:24 -0300

    010806042023

--- pages/api/login/index.ts
+++ pages/api/login/index.ts
@@ -5,7 +5,7 @@ type CorsMiddleware = (handler: NextApiHandler) => (req: NextApiRequest, res: Ne
 
 const allowCors: CorsMiddleware = (fn) => async (req, res) => {
   res.setHeader("Access-Control-Allow-Credentials", 'true');
-  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader('Access-Control-Allow-Origin', req.headers.origin as any);
   // another common pattern
   // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
   res.setHeader("Access-Control-Allow-Methods", "GET,OPTIONS,PATCH,DELETE,POST,PUT");
