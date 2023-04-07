776c10f9ea5038cf626156b1032bf2cd62035921
Esquiafo <leocapone@hotmail.com>
2023-04-07 02:15:06 -0300

    021507042023

--- next-env.d.ts
+++ next-env.d.ts
@@ -3,3 +3,9 @@
 
 // NOTE: This file should not be edited
 // see https://nextjs.org/docs/basic-features/typescript for more information.
+
+declare module '*.scss' {
+    const content: { [className: string]: string };
+    export default content;
+  }
+  
