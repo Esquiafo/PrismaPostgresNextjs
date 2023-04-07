b81c1536704bf7da5489d2cfa794a4f7260c8a9c
Esquiafo <leocapone@hotmail.com>
2023-04-07 03:13:33 -0300

    031307042033

--- pages/api/index.ts
+++ pages/api/index.ts
@@ -2,7 +2,7 @@ import { NextApiRequest, NextApiResponse } from 'next';
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.headers.origin !== 'http://localhost:3000') {
-    return res.status(200).json(req.headers);
+    return   res.status(200).json( req.headers['sec-fetch-site'] );
   }
 
   // Handle the API request here
